ebaca16d71e66baeea26bc46b01739e30ee24481
linct96 <linct196@gmail.com>
2022-05-02 00:08:20 +0800

    bump version

--- overrides.js
+++ overrides.js
@@ -1,14 +1,14 @@
 module.exports = {
   rules: {
     // 禁用冲突规则
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    // '@typescript-eslint/explicit-module-boundary-types': 'error'
   },
   overrides: [
     {
       // 只在 ts 中启用
       files: ['**/*.ts'],
       rules: {
-        '@typescript-eslint/explicit-module-boundary-types': ['error']
+        // '@typescript-eslint/explicit-module-boundary-types': ['error']
       }
     }
   ]
